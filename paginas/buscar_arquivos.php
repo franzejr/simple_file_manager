df9c1532eafb1bf457d11cd5da002004b0864259
Franzé Jr <franzejr@gmail.com>
2012-09-03 20:39:27 -0300

    [#master] Busca funcionando ok

--- paginas/buscar_arquivos.php
+++ paginas/buscar_arquivos.php
@@ -22,6 +22,21 @@
 			</div>
 			<input type="text" name="descricao" id="descricao" value="<?php getParameter('descricao'); ?>"/>
 		</div>
+		
+		<div>
+			<select name="categoria" id="categoria">				
+		<?
+			$sql = mysql_query("SELECT id_cat, nome FROM categoria") or die(mysql_error());
+			for($i=0;$i<mysql_num_rows($sql);$i++){
+				$nome = mysql_result($sql, $i, "nome");
+				$id_cat = mysql_result($sql,$i,"id_cat");
+		?>
+				<option><?=$id_cat?>-<?=$nome?></option>
+			<? } ?>
+			</select>
+		</div>
+		
+		
 		<input type="hidden" id="pag" name="pag" value="buscar_arquivos" />
 		<input type="submit" value="Buscar" />
 	</form>
@@ -31,25 +46,41 @@
 
 		//Se nenhum criterio de busca foi colocado, mostramos todos
 		if (!isset( $_GET["nome"]) && !isset( $_GET["descricao"]) ) {
-	    		echo "Nenhuma Pesquisa";
+	    		echo "Nenhuma Pesquisa feita.";
 			}
 		//Se um dos campos que queremos buscar foi escrito
-		if (  isset($_GET["nome"]) || isset($_GET["descricao"])  ) {
+		if (  isset($_GET["nome"]) || isset($_GET["descricao"]) || isset($_GET["categoria"])  ) {
 			echo "Algumas Pesquisas foram encontradas:";
 			
 			$nome = $_GET["nome"];
 			$descricao = $_GET["descricao"];
+			$categoria = $_GET["categoria"];
+			$id_cat_vet = split("-",$categoria);
+			$id_cat = $id_cat_vet[0];
+			
 
 			if ($nome != NULL && $descricao != NULL ) {
-				$comando = "SELECT nome,caminho, descricao FROM arquivo WHERE nome LIKE ('$titulo%') and descricao LIKE ('$descricao%')";
+				$comando = "SELECT nome,caminho, descricao FROM arquivo WHERE nome LIKE ('$nome%') and descricao LIKE ('$descricao%')";
 			}
 
 			if ($nome != NULL && $descricao == NULL ) {
-				$comando = "SELECT nome,caminho, descricao FROM arquivo WHERE nome LIKE ('$titulo%')";
+				$comando = "SELECT nome,caminho, descricao FROM arquivo WHERE nome LIKE ('$nome%')";
 			}
 
 			if ($nome == NULL && $descricao != NULL ) {
 				$comando = "SELECT nome,caminho, descricao FROM arquivo WHERE descricao LIKE ('$descricao%')";
+			}
+			
+			if($categoria != NULL && $descricao == NULL && $nome == NULL){
+				$comando = "SELECT nome, caminho,descricao FROM arquivo a, arq_cat ac where a.id_arq = ac.id_arq and ac.id_cat = $id_cat";
+			}
+			
+			if($categoria != NULL && $descricao != NULL && $nome == NULL){
+				$comando = "SELECT nome, caminho,descricao FROM arquivo a, arq_cat ac where a.id_arq = ac.id_arq and ac.id_cat = $id_cat and a.descricao LIKE ('$descricao%')";
+			}
+			
+			if($categoria != NULL && $descricao != NULL && $nome != NULL){
+				$comando = "SELECT nome, caminho,descricao FROM arquivo a, arq_cat ac where a.id_arq = ac.id_arq and ac.id_cat = $id_cat and a.descricao LIKE ('$descricao%') and a.nome LIKE ('$nome%')";
 			}
 
 			$sql = mysql_query($comando) or die(mysql_error());

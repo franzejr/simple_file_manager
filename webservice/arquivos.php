4bc4e987cbdd5553a44f86f2b5a20c0171adca09
Franzé Jr <franzejr@gmail.com>
2012-09-11 21:40:03 -0300

    [#master] Colocando conexao com o webservice em forma de include

--- webservice/arquivos.php
+++ webservice/arquivos.php
@@ -1,26 +1,5 @@
 <?php
-	/* CONFIGURACOES DO BANCO 
-		Coloque as configuracoes abaixo de acordo com o seu MySQL
-	*/
-	$usuario = "usuario";
-	$host = "localhost";
-	$bd = "trab3_franzejr";
-	$user = "root";
-	$password = "";
-	
-	
-	/* CONECTANDO AO BANCO */
-	$connection = mysql_connect($host, $user,$password);
-	
-	if (!($connection)){
-			die("Error: ". mysql_error());
-	}
-	mysql_set_charset('utf8',$connection); //Setting the utf-8 on the database
-		
-	$bd_connect = mysql_select_db($bd, $connection); //rf services
-	if (!($bd_connect)){
-		die("Error: ". mysql_error());
-	}
+	include("../banco/conecta.php");
 	
 	/*Configuracoes do webservice */
 
@@ -47,7 +26,7 @@
 	  } 
 	
 	if($format == 'json') {
-	    header('Content-type: application/json');
+	    //header('Content-type: application/json');
 		echo json_encode($arquivos);
 	  }
 		else {
